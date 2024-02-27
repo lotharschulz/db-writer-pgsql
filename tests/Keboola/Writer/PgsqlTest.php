b312226afa6c439dd03dbb9365907705c48857dd
Výborný Adam <mail@adamvyborny.cz>
2024-02-27 15:34:59 +0100

    Update libpq-dev library

--- tests/Keboola/Writer/PgsqlTest.php
+++ tests/Keboola/Writer/PgsqlTest.php
@@ -143,7 +143,7 @@ class PgsqlTest extends BaseTest
         $this->writer->drop($table['dbName']);
         $this->writer->create($table);
         $this->assertTrue(
-            $this->logHandler->hasInfoThatContains('PgSQL server version: 15.1')
+            $this->logHandler->hasInfoThatMatches('/PgSQL server version: \d+/')
         );
         $this->assertTrue(
             $this->logHandler->hasInfoThatContains(sprintf('CREATE TABLE IF NOT EXISTS "%s"', $table['dbName']))
